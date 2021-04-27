3a2813b1d752926577ea3b74423bc3a5bf48f7b0
Revincx <revincx@outlook.com>
2021-04-27 22:06:38 +0800

    Add more pictures

--- learn.js
+++ learn.js
@@ -1,37 +1,21 @@
 const axios = require('axios')
+const fs = require('fs')
 
-// axios.defaults.baseURL = 'https://httpbin.org'
-// axios({
-//     url: '/get',
-//     method: 'get',
-//     params: {
-//         a: '1',
-//         b: 'jssd'
-//     }
-// })
-// .then(res => {
-//     console.log(res);
-// })
-
-const axiosA = axios.create({
-    baseURL: 'https://httpbin.org/',
-    timeout: '10000'
-})
-
-const interceptor = axiosA.interceptors.request.use(res => {
-
-}, error => {
-
-})
-
-axiosA.interceptors.request.eject(interceptor)
-
-axiosA.get('/get',{
-    params: {
-        a: 1,
-        b: 'hhs'
-    }
-})
-.then(res => {
-    console.log(res.data);
-})
+for(let i =0;i < 426;i++)
+{
+    axios.get('https://api.xhofe.top/img/api/',
+        {
+            params: {
+                type: 'json',
+                id: i
+            }
+        }
+    ).then(response => {
+        // let obj = JSON.parse(response.data.toString())
+        // console.log(obj.url)
+        console.log(`${i}: ${response.data.url}`)
+        fs.writeFile(__dirname + '/images.txt',response.data.url + '\n',{
+            flag: 'a+'
+        },err => { })
+    })
+}
